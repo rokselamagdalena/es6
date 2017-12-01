82971deb2fd1a01ebc0cc6431cc22431506ed5f7
rokselamagdalena <rokselamagdalena@gmail.com>
2017-12-01 13:20:17 +0100

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,43 @@
+var path = require('path');
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+var ExtrectTextPlugin = require('extract-text-webpack-plugin');
+
+var extrectPlugin = new ExtrectTextPlugin({
+    filename: 'main.css'
+});
+
 module.exports = {
     entry: "./main.js",
     output: {
-        path: __dirname,
-        filename: "bundle.js"
+        path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.js",
+        publicPath: "/dist"
     },
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
+                test: /\.js$/,
+                use:
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
 
-                ]
+                    }
+            },
+            {
+                test: /\.scss$/,
+                use: extrectPlugin.extract({
+                    use: ['css-loader', 'sass-loader']
+                })
             }
         ]
     },
     plugins: [
         new OpenBrowserPlugin({
             url: 'http://localhost:8080'
-        })
+        }),
+        extrectPlugin
     ]
 };
